9b9870e9455ccd44623416e87a3d4f41a57645f7
Habashy <t-habashi@cherpa.co.jp>
2023-02-13 17:12:44 +0900

    [update] TodoListにチェックボックスを追加

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <!-- 参考リンク -->
-<!-- https://ja.vuejs.org/tutorial/#step-7 -->
+<!-- https://ja.vuejs.org/tutorial/#step-8 -->
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed} from 'vue'
+
 
 let id = 0
 
 const newTodo = ref('')
+const hideCompleted = ref(false)
 
-//id++することでn+1できて便利
+//TodoListのデータ
 const todos = ref([
-  { id: id++, text: 'Learn HTML'},
-  { id: id++, text: 'Learn Javascript'},
-  { id: id++, text: 'Learn Vue'}
+  { id: id++, text: 'Learn HTML', done: true},
+  { id: id++, text: 'Learn Javascript', done: true},
+  { id: id++, text: 'Learn Vue', done: false}
 ])
 
+//TodoListのDoneを切り替える
+const filteredTodos = computed(() => { //computed()ってなんやねん
+  return hideCompleted.value
+    ? todos.value.filter((t)=>!t.done)
+    : todos.value
+})
+
+//TodoListにTodoを追加
 const addTodo=()=>{
-  todos.value.push({id: id++, text: newTodo.value})
+  todos.value.push({id: id++, text: newTodo.value, done:false})
   newTodo.value='' //値の初期化
 }
 
+//TodoListのTodoを削除
 const removeTodo=(todo)=>{
   todos.value = todos.value.filter((t) => t !== todo) //t とは → filterの返り値
 }
@@ -31,11 +42,22 @@ const removeTodo=(todo)=>{
     <button>Add Todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{done : todo.done}">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
+
+<style>
+  .done{
+    text-decoration: line-through;
+  }
+</style>
 
 
