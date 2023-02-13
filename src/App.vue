39a4adc73ff00720f5b58c670b98be0b626de66e
Habashy <t-habashi@cherpa.co.jp>
2023-02-13 16:10:09 +0900

    [update] ref と reactive の使い方

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
-<template>
-  <h1>HelloWorld</h1>
-</template>
+<script setup>
+import { reactive, ref } from 'vue'
 
-<!-- <script>
-import HelloWorld from './components/HelloWorld.vue'
+//reactive の使い方
+const counter = reactive({
+  count:0
+})
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+console.log(counter.count)
+counter.count++
+
+//refの使い方
+const message = ref("Hello World")
+//文字を代入する関数か？
+console.log(message)
+message.value = "Changed"
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
+<template>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
+
+<!-- 参考リンク -->
+<!-- https://ja.vuejs.org/tutorial/#step-2 -->
